befced83f2ce3287771f5bdc1dab6e225994bbcb
Oleg <olegparamey4@gmail.com>
2024-06-23 14:59:58 +0200

    added some practice code with create-react-app, tried: npm start; npm run build - npx serve -s build; npm test

--- react-projects/06-create-react-app/src/App.js
+++ react-projects/06-create-react-app/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello from Create React App</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +17,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
