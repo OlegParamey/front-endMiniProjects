a87d99592654ca174f9848574d379e8a3e6264c7
Oleg <olegparamey4@gmail.com>
2024-06-24 20:06:03 +0200

    added miniproject with reusable components using modules to import and export them

---
+++ react-projects/08-reusable-components/src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import MyComponent from './components/MyComponent.js'
+import OtherComponent from './components/OtherComponent.js'
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <OtherComponent />
+      <MyComponent />
+      <OtherComponent />
+      <MyComponent />
+      <OtherComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ react-projects/08-reusable-components/src/components/OtherComponent.js
@@ -0,0 +1,9 @@
+function OtherComponent() {
+  return (
+    <div>
+      <p>Other component is here!</p>
+    </div>
+  )
+}
+
+export default OtherComponent
