9d20b98ba348ab8c29568f7830da111494c962a0
Oleg <olegparamey4@gmail.com>
2024-06-26 19:29:04 +0200

    modified project with adding functionality to it, increasing a number by 1 by clicking a button

--- react-projects/12-state-via-props/src/App.js
+++ react-projects/12-state-via-props/src/App.js
@@ -1,7 +1,23 @@
+import { useState } from 'react'
 import './App.css'
+import Button from './components/Button'
+import Counter from './components/Counter'
 
 function App() {
-  return <div className="App"></div>
+  const [count, setCount] = useState(0)
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div className="App">
+      <Counter number={count} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+    </div>
+  )
 }
 
 export default App
