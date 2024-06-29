348055d7f890fa67987a2acb170487cf5183e3af
Oleg <olegparamey4@gmail.com>
2024-06-29 18:03:59 +0200

    some project with React Fragment

---
+++ react-projects/16-react-fragment/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <React.Fragment>
+      <h1>Oleg</h1>
+      <h3>I'm practicing</h3>
+      <button>Like!</button>
+    </React.Fragment>
+  )
+}
+
+export default Card
