ed3e80565dce9c4f0bac2c61a158f893fbd373c3
Oleg <olegparamey4@gmail.com>
2024-07-08 14:55:04 +0200

    added Routes to App

--- react-projects/26-react-router/src/App.js
+++ react-projects/26-react-router/src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="about" element={<h1>About</h1>} />
+          <Route path="contacts" element={<h1>Contacts</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
