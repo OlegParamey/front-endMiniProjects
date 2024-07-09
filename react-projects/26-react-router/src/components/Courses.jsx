ad3bd7a396ba5eb1a234d0d7c4d98ff68a9b020f
Oleg <olegparamey4@gmail.com>
2024-07-09 23:17:49 +0200

    added Courses list sorting based on query string

--- react-projects/26-react-router/src/components/Courses.jsx
+++ react-projects/26-react-router/src/components/Courses.jsx
@@ -1,10 +1,33 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import queryString from 'query-string'
 import courses from '../data/courses'
+import { useEffect } from 'react'
 
 const Courses = () => {
+  //location = {prop: "val"}
+  const location = queryString.parse(useLocation().search)
+  const navigate = useNavigate()
+  let isSorted = false
+
+  useEffect(() => {
+    if (!Object.hasOwn(courses[0], `${location.sort}`)) {
+      navigate('.', { relative: 'path' })
+    }
+  }, [location.sort, navigate])
+
+  if (location.sort) {
+    courses.sort((a, b) => {
+      if (a[`${location.sort}`] < b[`${location.sort}`]) return -1
+      if (a[`${location.sort}`] > b[`${location.sort}`]) return 1
+      return 0
+    })
+    isSorted = true
+  }
+  console.log(courses)
+
   return (
     <>
-      <h1>Courses</h1>
+      <h1>{isSorted ? `Courses sorted by ${location.sort}` : `Courses`}</h1>
       {courses.map((course) => (
         <div key={course.id}>
           <Link to={course.slug} className="courseLink">
--- react-projects/26-react-router/src/components/SingleCourse.jsx
+++ react-projects/26-react-router/src/components/SingleCourse.jsx
@@ -1,14 +1,24 @@
-import { Link, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import courses from '../data/courses'
+import { useEffect } from 'react'
 
 function SinglePageCourse() {
   const { slug } = useParams()
+  const navigate = useNavigate()
+
   const currentCourse = courses.find((obj) => obj.slug === slug)
+
+  useEffect(() => {
+    if (!currentCourse) {
+      navigate('..', { relative: 'path' })
+    }
+  }, [currentCourse, navigate])
+
   return (
     <>
-      <h1>{currentCourse.title}</h1>
-      <h2>{currentCourse.slug}</h2>
-      <h3>{currentCourse.id}</h3>
+      <h1>{currentCourse?.title}</h1>
+      <h2>{currentCourse?.slug}</h2>
+      <h3>{currentCourse?.id}</h3>
       <Link to=".." relative="path">
         All Courses
       </Link>
