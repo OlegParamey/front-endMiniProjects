e5640c44a18e00af24241631ee5e988edfcd92af
Oleg Paramey <olegparamey4@gmail.com>
2024-07-21 19:28:24 +0200

    added new action type, added new action creator - DELETE_BOOK, added new case handler to reducer, added handlerDeleteBook function to the BookList

--- react-projects/30-react-redux/frontend/src/redux/books/actionCreators.js
+++ react-projects/30-react-redux/frontend/src/redux/books/actionCreators.js
@@ -6,3 +6,7 @@ export const addBook = (newBook) => {
     payload: newBook,
   }
 }
+
+export const deleteBook = (id) => {
+  return { type: actionTypes.DELETE_BOOK, payload: id }
+}
--- react-projects/30-react-redux/frontend/src/redux/books/reducer.js
+++ react-projects/30-react-redux/frontend/src/redux/books/reducer.js
@@ -6,6 +6,8 @@ const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_BOOK:
       return [...state, action.payload]
+    case actionTypes.DELETE_BOOK:
+      return state.filter((book) => book.id !== action.payload)
     default:
       return state
   }
