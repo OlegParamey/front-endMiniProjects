986ecfebf40962da77bd6be6c200fa7ca3fb556b
Oleg Paramey <olegparamey4@gmail.com>
2024-07-21 21:59:12 +0200

    created file utils, added there function createBookWithId and used it in BookForm

--- react-projects/30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ react-projects/30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { v4 as uuidv4 } from 'uuid'
 import { addBook } from '../../redux/books/actionCreators'
 import booksData from '../../data/books.json'
+import createBookWithId from '../../utils/createBookWithId'
 import './BookForm.css'
 
 function BookForm() {
@@ -14,25 +14,14 @@ function BookForm() {
     const randomIndex = Math.floor(Math.random() * booksData.length)
     const randomBook = booksData[randomIndex]
 
-    const randomBookWithID = {
-      ...randomBook,
-      isFavorite: false,
-      id: uuidv4(),
-    }
-    dispatch(addBook(randomBookWithID))
+    dispatch(addBook(createBookWithId(randomBook)))
   }
 
   function handleSubmit(event) {
     event.preventDefault()
 
     if (title && author) {
-      const book = {
-        title,
-        author,
-        isFavorite: false,
-        id: uuidv4(),
-      }
-      dispatch(addBook(book))
+      dispatch(addBook(createBookWithId({ title, author })))
       setAuthor('')
       setTitle('')
     }
