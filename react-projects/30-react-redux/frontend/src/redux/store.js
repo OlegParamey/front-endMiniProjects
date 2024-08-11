7d9b83fdcc37a8eee7b4553a6e04cf718ea469af
Oleg Paramey <olegparamey4@gmail.com>
2024-08-11 15:30:17 +0200

    Added errorSlice

--- react-projects/30-react-redux/frontend/src/redux/store.js
+++ react-projects/30-react-redux/frontend/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import bookReducer from './slices/bookSlice'
 import filterReducer from './slices/filterSlice'
+import errorReducer from './slices/errorSlice'
 
 const store = configureStore({
-  reducer: {
-    books: bookReducer,
-    filter: filterReducer,
-  },
+    reducer: {
+        books: bookReducer,
+        filter: filterReducer,
+        error: errorReducer,
+    },
 })
 
 export default store
