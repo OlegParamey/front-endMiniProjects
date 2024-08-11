5c338aad1ed862555acfbb3705f498ede5371edb
Oleg Paramey <olegparamey4@gmail.com>
2024-08-11 16:33:56 +0200

    dispatch error in the async thunk

--- react-projects/30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ react-projects/30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -31,7 +31,7 @@ function BookForm() {
     }
 
     const handleAddRandomBookViaAPI = () => {
-        dispatch(fetchBook())
+        dispatch(fetchBook('http://localhost:4000/random-book'))
     }
 
     return (
