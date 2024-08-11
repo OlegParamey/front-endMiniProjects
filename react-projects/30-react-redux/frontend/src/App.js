94fbcf84f7f982173ec9d6fd84632587e5aaf586
Oleg Paramey <olegparamey4@gmail.com>
2024-08-11 16:04:54 +0200

    Added react-toastify package and Error component

--- react-projects/30-react-redux/frontend/src/App.js
+++ react-projects/30-react-redux/frontend/src/App.js
@@ -1,25 +1,27 @@
 import BookForm from './components/BookForm/BookForm'
 import BookList from './components/BookList/BookList'
 import Filter from './components/Filter/Filter'
+import Error from './components/Error/Error'
 import './App.css'
 
 function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1>Book Library App</h1>
-      </header>
-      <main className="app-main">
-        <div className="app-left-column">
-          <BookForm />
+    return (
+        <div className="app">
+            <header className="app-header">
+                <h1>Book Library App</h1>
+            </header>
+            <main className="app-main">
+                <div className="app-left-column">
+                    <BookForm />
+                </div>
+                <div className="app-right-column">
+                    <Filter />
+                    <BookList />
+                </div>
+            </main>
+            <Error />
         </div>
-        <div className="app-right-column">
-          <Filter />
-          <BookList />
-        </div>
-      </main>
-    </div>
-  )
+    )
 }
 
 export default App
--- react-projects/30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ react-projects/30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,68 +1,71 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addBook, fetchBook } from '../../redux/slices/bookSlice'
+import { setError } from '../../redux/slices/errorSlice'
 import booksData from '../../data/books.json'
 import createBookWithId from '../../utils/createBookWithId'
 import './BookForm.css'
 
 function BookForm() {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const dispatch = useDispatch()
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const dispatch = useDispatch()
 
-  function handleAddRandomBook() {
-    const randomIndex = Math.floor(Math.random() * booksData.length)
-    const randomBook = booksData[randomIndex]
+    function handleAddRandomBook() {
+        const randomIndex = Math.floor(Math.random() * booksData.length)
+        const randomBook = booksData[randomIndex]
 
-    dispatch(addBook(createBookWithId(randomBook, 'random')))
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault()
-
-    if (title && author) {
-      dispatch(addBook(createBookWithId({ title, author }, 'manual')))
-      setAuthor('')
-      setTitle('')
+        dispatch(addBook(createBookWithId(randomBook, 'random')))
     }
-  }
 
-  const handleAddRandomBookViaAPI = () => {
-    dispatch(fetchBook())
-  }
+    function handleSubmit(event) {
+        event.preventDefault()
 
-  return (
-    <div className="app-block book-form">
-      <h2>Add a New Book</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title: </label>
-          <input
-            type="text"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          ></input>
+        if (title && author) {
+            dispatch(addBook(createBookWithId({ title, author }, 'manual')))
+            setAuthor('')
+            setTitle('')
+        } else {
+            dispatch(setError('You must fill title and author!'))
+        }
+    }
+
+    const handleAddRandomBookViaAPI = () => {
+        dispatch(fetchBook())
+    }
+
+    return (
+        <div className="app-block book-form">
+            <h2>Add a New Book</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title: </label>
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    ></input>
+                </div>
+                <div>
+                    <label htmlFor="author">Author: </label>
+                    <input
+                        type="text"
+                        id="author"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                    ></input>
+                </div>
+                <button type="submit">Add Book</button>
+                <button type="button" onClick={handleAddRandomBook}>
+                    Add Random Book
+                </button>
+                <button type="button" onClick={handleAddRandomBookViaAPI}>
+                    Add Random via API
+                </button>
+            </form>
         </div>
-        <div>
-          <label htmlFor="author">Author: </label>
-          <input
-            type="text"
-            id="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          ></input>
-        </div>
-        <button type="submit">Add Book</button>
-        <button type="button" onClick={handleAddRandomBook}>
-          Add Random Book
-        </button>
-        <button type="button" onClick={handleAddRandomBookViaAPI}>
-          Add Random via API
-        </button>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default BookForm
