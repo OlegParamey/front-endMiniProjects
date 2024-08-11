646a89c4890f47f5c73acbbd8689fd07e738fdbd
Oleg Paramey <olegparamey4@gmail.com>
2024-08-11 16:58:27 +0200

    Another option for extraReducers

--- react-projects/30-react-redux/frontend/src/redux/slices/bookSlice.js
+++ react-projects/30-react-redux/frontend/src/redux/slices/bookSlice.js
@@ -36,6 +36,16 @@ const bookSlice = createSlice({
             )
         },
     },
+    //OPTION 1
+    // extraReducers: {
+    //     [fetchBook.fulfilled]: (state, action) => {
+    //         if (action.payload.title && action.payload.author) {
+    //             state.push(createBookWithId(action.payload, 'API'))
+    //         }
+    //     },
+    // },
+
+    //OPTION 2
     extraReducers: (builder) => {
         builder.addCase(fetchBook.fulfilled, (state, action) => {
             if (action.payload.title && action.payload.author) {
